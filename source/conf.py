5dea64f88585027baa1fdb72fa1e8d6af9005845
ChucklesOnGitHub <ceci.herbert@hotmail.com>
2022-09-15 12:06:33 +0100

    corrected reference to logo

--- source/conf.py
+++ source/conf.py
@@ -78,7 +78,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 html_theme = "pydata_sphinx_theme"
-html_logo = "_static/images/oe_logo_template.svg"
+html_logo = "_static/images/oe_logo_name.svg"
 html_scaled_image_link = True
 
 
